cae6c9365d3dc1771da148c21e1df29d6e0ceff6
Erin Stewart <dragonkittyrawr@users.noreply.github.com>
2017-04-28 21:53:08 -0500

    Changed port server listens on.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-var port = 8080;
+// var port = 8080;
 
 var app = express();
 
@@ -25,4 +25,4 @@ var routes = require("./controllers/burgers_controller.js");
 
 app.use("/", routes);
 
-app.listen(port);
+app.listen(process.env.PORT || 8080);
